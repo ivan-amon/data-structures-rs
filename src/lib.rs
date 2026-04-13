2b344068df1fe818b5cb6732adcf61c19da2354d
Ivan <iab050811@gmail.com>
2026-04-13 23:05:30 +0200

    Linked List

---
+++ src/lib.rs
@@ -0,0 +1,2 @@
+pub mod linked_list;
+pub use linked_list::LinkedList;
---
+++ src/linked_list.rs
@@ -0,0 +1,41 @@
+struct Node<T> {
+    value: T,
+    next: Option<Box<Node<T>>>,
+}
+
+pub struct LinkedList<T> {
+    head: Option<Box<Node<T>>>,
+    size: usize,
+}
+
+impl<T> LinkedList<T> {
+
+    pub fn new() -> Self {
+        LinkedList { head: None, size: 0 }
+    }
+
+    pub fn size(&self) -> usize { self.size }
+
+    pub fn push(&mut self, value: T) { // O(1)
+        let old = self.head.take();
+        let new_node = Node {
+            value,
+            next: old,
+        };
+        self.head = Some(Box::new(new_node));
+        self.size += 1;
+    }
+
+    pub fn pop(&mut self) -> Option<T> { // O(1)
+        let old = self.head.take();
+        match old {
+            Some(old) => {
+                let popped = old.value;
+                self.head = old.next;
+                self.size -= 1;
+                Some(popped)
+            },
+            None => None,
+        }
+    }
+}
